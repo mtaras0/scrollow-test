40f9fbbe1fba37d59bf44da535bee3e37e0a213e
Taras Mukin <mukintaras@gmail.com>
2023-03-30 14:07:17 +0600

    deploy

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/scrollow/sw.js', { scope: '/scrollow/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/scrollow-test2/sw.js', { scope: '/scrollow-test2/' })})}
